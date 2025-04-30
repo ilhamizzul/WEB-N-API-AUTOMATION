5890eccf0bd9258488176973ba6ed9518de1f878
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2025-04-30 16:35:26 +0700

    Remove Allure integration and refine reporting setup
    Allure-related configurations and annotations were removed from tests and Gradle files to simplify the reporting process. Updated test report generation to use basic HTML output and adjusted CI workflows to handle the new report paths and naming conventions.

--- src/test/java/api/tests/UserTest.java
+++ src/test/java/api/tests/UserTest.java
@@ -12,11 +12,6 @@ public class UserTest {
     private UserApi userApi = new UserApi();
 
     @Test(description = "Test Get Users", groups = {"api"})
-    @Epic("User Management")
-    @Feature("Get Users")
-    @Story("Valid Get User List")
-    @Severity(SeverityLevel.NORMAL)
-//    @Step("Sending GET request to /user")
     public void testGetAllUser() {
         Response res = userApi.getListUsers();
         Assert.assertEquals(res.getStatusCode(), 200);
