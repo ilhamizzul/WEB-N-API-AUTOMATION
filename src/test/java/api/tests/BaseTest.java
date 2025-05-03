812632159e5262980f1e5cdb37cb7f55300ee66d
ilhamizzul <ilhamizzul@gmail.com>
2025-05-03 21:49:02 +0700

    Add Tag API and test for retrieving all tags
    Introduced a new `TagApi` class with a method to fetch tags. Updated `BaseTest` to include `TagApi` and added a new `TagTest` class to validate the API response with status code and response time assertions.

--- src/test/java/api/tests/BaseTest.java
+++ src/test/java/api/tests/BaseTest.java
@@ -1,7 +1,9 @@
 package api.tests;
 
+import api.apis.TagApi;
 import api.apis.UserApi;
 
 public class BaseTest {
     protected UserApi userApi = new UserApi();
+    protected TagApi tagApi = new TagApi();
 }
