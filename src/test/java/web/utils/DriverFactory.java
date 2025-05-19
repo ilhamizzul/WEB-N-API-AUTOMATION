0888a6aac66b3377006d370bbf8c1d5a7a936b47
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2025-05-19 11:28:38 +0700

    Refactor DriverFactory to support multiple browsers.
    Updated `getDriver` to accept a browser parameter and initialize the driver accordingly. Added method overloading to set a default browser using a system property if none is specified.

--- src/test/java/web/utils/DriverFactory.java
+++ src/test/java/web/utils/DriverFactory.java
@@ -5,12 +5,30 @@ import org.openqa.selenium.WebDriver;
 public class DriverFactory {
     private static WebDriver driver;
 
-    public static WebDriver getDriver() {
+    public static WebDriver getDriver(String browser) {
         if (driver == null) {
-            driver = WebDriverConfig.initChromeDriver();
+            switch (browser.toLowerCase()) {
+                case "edge":
+                    driver = WebDriverConfig.initEdgeDriver();
+                    break;
+                case "firefox":
+                    driver = WebDriverConfig.initFirefoxDriver();
+                    break;
+                case "chrome":
+                default:
+                    driver = WebDriverConfig.initChromeDriver();
+                    break;
+            }
+
         }
         return driver;
     }
+
+    public static WebDriver getDriver() {
+        String browser = System.getProperty("browser", "firefox");
+        return getDriver(browser);
+    }
+
 
     public static void quitDriver() {
         if (driver != null) {
